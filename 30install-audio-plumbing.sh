d034199a036552b6d5c12c6684e4f9b58dfdfa52
M. Edward (Ed) Borasky <znmeb@algocompsynth.com>
2021-03-20 18:27:56 -0700

    audio plumbing installer done

---
+++ 30install-audio-plumbing.sh
@@ -0,0 +1,78 @@
+#! /bin/bash
+
+# Copyright (C) 2021 M. Edward (Ed) Borasky <mailto:znmeb@algocompsynth.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# http://tedfelix.com/linux/linux-midi.html
+echo "Installing L4T audio plumbing packages"
+echo "Be sure to say 'yes' when it asks if you want to enable realtime process priority:"
+sudo apt-get install -y \
+  alsa-tools \
+  flac \
+  fluid-soundfont-gm \
+  fluid-soundfont-gs \
+  fluidsynth \
+  jackd2 \
+  jack-tools \
+  jacktrip \
+  libasound2-dev \
+  libbluetooth-dev \
+  libcamera-calibration-parsers-dev \
+  libcamera-info-manager-dev \
+  libdbus-1-dev \
+  libfdk-aac-dev \
+  libjack-jackd2-dev \
+  libncurses5-dev \
+  libsbc-dev \
+  libsdl2-dev \
+  libsndfile1-dev \
+  libsox-fmt-all \
+  libsystemd-dev \
+  libudev-dev \
+  libvulkan-dev \
+  mp3splt \
+  pmidi \
+  sox \
+  timidity
+
+source $HOME/miniconda3/etc/profile.d/conda.sh
+
+echo "Creating fresh 'pipewire' conda environment"
+conda env remove --name pipewire --yes
+conda create --name pipewire --quiet --yes \
+  meson \
+  ninja
+conda activate pipewire
+export MESON_OPTIONS_PATCH=$PWD/meson_options.txt
+mkdir --parents $CONDA_PREFIX/src
+pushd $CONDA_PREFIX/src
+  rm -fr pipewire*
+  export PIPEWIRE_VERSION="0.3.24"
+
+  echo "Cloning 'pipewire' repo"
+  git clone https://gitlab.freedesktop.org/pipewire/pipewire.git
+  cd pipewire
+  git checkout $PIPEWIRE_VERSION
+
+  echo "Disabling 'alsa' interfaces - L4T alsa libraries are too old"
+  diff $MESON_OPTIONS_PATCH meson_options.txt || true
+  cp $MESON_OPTIONS_PATCH meson_options.txt
+
+  echo "Building 'pipewire'"
+  meson setup build
+  meson configure build
+  meson configure -Dprefix=$CONDA_PREFIX
+  ninja -C build
+  popd
--- README.md
+++ README.md
@@ -3,12 +3,7 @@ Main code repository for AlgoCompSynth One
 
 ## Setup
 
-1. To tell the `vim` editor whether your terminal background is light or dark, enter "./10set-vim-background.sh".
-2. To disable the display manager / desktop and reboot, enter "./12disable-desktop.sh". You will need to enter your password.
-3. To configure the base audio, enter "14configure-audio.sh". You will need to enter your password. This will
-
-    a. Install base audio packages,
-    b. Enable Bluetooth audio, and
-    c. Reboot.
-
-    One of the audio packages will ask you if you want to enable realtime priority. The correct answer is "yes".
+1. To tell the `vim` editor whether your terminal background is light or dark, enter "./00set-vim-background.sh".
+2. To disable the display manager / desktop and reboot, enter "./02disable-desktop.sh". You will need to enter your password.
+3. To install Miniforge, enter "./20install-miniforge.sh".
+4. To install the audio plumbing, enter "30-install-audio-plumbing.sh". You will need to enter your password. One of the audio packages will ask you if you want to enable realtime priority. The correct answer is "yes".
