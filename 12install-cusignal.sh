4829548711ddd0315b2a2ff3fc9559775507fc62
M. Edward (Ed) Borasky <znmeb@algocompsynth.com>
2021-03-22 19:20:43 -0700

    fixed

--- 12install-cusignal.sh
+++ 12install-cusignal.sh
@@ -20,65 +20,59 @@ set -e
 # see https://github.com/edgyR/edgyR-containers/issues/31,
 # https://docs.cupy.dev/en/stable/reference/environment.html#for-installation,
 # and https://developer.nvidia.com/cuda-gpus
-echo "Setting 'CUDA' path"
-export CUDA_PATH=/usr/local/cuda-10.2
-export PATH=$PATH:$CUDA_PATH/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_PATH/targets/aarch64-linux/lib/
-export CPATH=$CPATH:$CUDA_PATH/targets/aarch64-linux/include/
-
-echo "Discovering CUDA capability"
-/usr/local/bin/deviceQuery \
-  | grep -e "CUDA Capability Major/Minor version number:" \
-  | sed 's;CUDA Capability Major/Minor version number:;;' \
-  | sed 's;\s;;g' \
-  | sed 's;\.;;' > /tmp/cuda_capability.txt
-export CUDA_CAPABILITY=`cat /tmp/cuda_capability.txt`
-export \
-  CUPY_NVCC_GENERATE_CODE="arch=compute_$CUDA_CAPABILITY,code=sm_$CUDA_CAPABILITY"
-echo "CUPY_NVCC_GENERATE_CODE = $CUPY_NVCC_GENERATE_CODE"
+export CUSIGNAL_VERSION=v0.18.0
 export CUPY_NUM_BUILD_JOBS=`nproc`
-export CONDA_ENV_FILE=$PWD/misc/cusignal_jetson_base.yml
-cat Rprofile >> ~/.Rprofile
+export CUSIGNAL_JETSON_BASE=$PWD/misc/cusignal_jetson_base.yml
+export INSTALLED_PACKAGES=$PWD/installed-packages.txt
+export AVAILABLE_R_PACKAGES=$PWD/available-R-packages.txt
 
 echo "Cloning 'cusignal'"
-export PROJECT_HOME=$HOME/Projects
-mkdir --parents $PROJECT_HOME
-pushd $PROJECT_HOME
+mkdir --parents $HOME/Projects
+pushd $HOME/Projects
 
-  export CUSIGNAL_VERSION=v0.18.0
   export CUSIGNAL_HOME=$(pwd)/cusignal
   rm -fr $CUSIGNAL_HOME
   git clone https://github.com/rapidsai/cusignal.git $CUSIGNAL_HOME
   cd $CUSIGNAL_HOME
   echo "Checking out version '$CUSIGNAL_VERSION'"
   git checkout $CUSIGNAL_VERSION
-
+  
   echo "Creating fresh 'r-reticulate' environment"
-  echo "This takes about 18 minutes on a Jetson Xavier NX"
+  echo "This takes about 45 minutes on a Jetson Xavier NX"
   source $HOME/miniconda3/etc/profile.d/conda.sh
-  conda env remove --name r-reticulate --quiet --yes
   /usr/bin/time conda env create --quiet --force --file \
-    "$CONDA_ENV_FILE"
-
+    $CUSIGNAL_JETSON_BASE
+  
   echo "Activating 'r-reticulate'"
   conda activate r-reticulate
+  
+  echo "Installing 'cusignal'"
+  export PATH=$PATH:/usr/local/cuda-10.2/bin
+  /usr/bin/time ./build.sh
+  echo "Copying '$CUSIGNAL_HOME/notebooks' to '$HOME/Notebooks/cusignal-notebooks'"
+  mkdir --parents $HOME/Notebooks
+  cp -rp $CUSIGNAL_HOME/notebooks $HOME/Notebooks/cusignal-notebooks
+
+  echo "Installing JupyterLab and R base"
+  conda install --quiet --yes \
+    jupyterlab \
+    pandas \
+    sympy \
+    r-base \
+    r-data.table \
+    r-renv \
+    r-irkernel \
+    r-reticulate
 
   echo "Installing R kernel in JupyterLab"
   Rscript -e "IRkernel::installspec()"
 
   echo "Installing R library package 'caracas'"
   Rscript -e "install.packages('caracas', quiet = TRUE)"
-  echo "Writing the R shopping list"
-  conda search 'r-' | grep ^r- | sed 's/ .*$//' | sort -u > r-shopping-list.txt
+  echo "Available R packages"
+  conda search 'r-' | grep ^r- | sed 's/ .*$//' | sort -u | tee $AVAILABLE_R_PACKAGES
+  
+  echo "Installed packages:"
+  conda list | tee $INSTALLED_PACKAGES
 
-  echo "Installing 'cusignal'"
-  export CUPY_NUM_BUILD_JOBS=`nproc`
-  /usr/bin/time ./build.sh
-  echo "Copying '$CUSIGNAL_HOME/notebooks' to '$HOME/Notebooks/cuSignal'"
-  mkdir --parents $HOME/Notebooks
-  cp -rp $CUSIGNAL_HOME/notebooks $HOME/Notebooks/cuSignal
-
-  echo "Installed packages"
-  conda list
-  conda deactivate
   popd
