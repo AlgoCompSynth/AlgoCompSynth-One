53b241a2e2b5e535b76a18743a38450514f7b33c
M. Edward (Ed) Borasky <znmeb@algocompsynth.com>
2021-03-24 23:28:57 -0700

    drop other softsynths till we get end-to-end livecoding done

--- docker-context/build.sh
+++ docker-context/build.sh
@@ -6,3 +6,4 @@ echo "Creating a backup - you can ignore missing images"
 sudo docker tag algocompsynth:latest algocompsynth:backup || true
 echo "Building 'algocompsynth'"
 /usr/bin/time sudo docker build --tag algocompsynth:latest .
+sudo docker images
