89cb593ec11f6d16eb9b6cab8a4301e6a224d8af
M. Edward (Ed) Borasky <znmeb@algocompsynth.com>
2021-04-02 01:23:49 -0700

    overnight smoke test

--- docker-contexts/all-images.sh
+++ docker-contexts/all-images.sh
@@ -2,20 +2,22 @@
 
 set -e
 
+./image.sh xpra > /tmp/i-xpra.log 2>&1
+./image.sh base > /tmp/i-base.log 2>&1
+
 for repo in \
-  base \
-  ml-base
+  audacity \
+  chuck \
+  csound-cuda
 do
   ./image.sh $repo > /tmp/i-$repo.log 2>&1 &
 done
 wait
 
 for repo in \
-  csound \
-  chuck
+  rstats-audio \
+  tidal
 do
   ./image.sh $repo > /tmp/i-$repo.log 2>&1 &
 done
 wait
-
-docker images
