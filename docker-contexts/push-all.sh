02dcc69c7954b8cfff02a45ca1008a4b67c30c0b
M. Edward (Ed) Borasky <znmeb@algocompsynth.com>
2021-04-06 18:30:28 -0700

    fix bad repo tags

--- docker-contexts/push-all.sh
+++ docker-contexts/push-all.sh
@@ -8,10 +8,10 @@ for image in \
   docker.io/algocompsynth/algocompsynth-1 \
   docker.io/algocompsynth/algocompsynth-1r
 do
-  sudo docker push "synth/$image:latest"
+  sudo docker push "$image:latest"
   if [ ${#SYNTH_RELEASE} -gt "0" ]
   then 
-    sudo docker tag "synth/$image:latest" "synth/$image:$SYNTH_RELEASE"
-    sudo docker push "synth/$image:$SYNTH_RELEASE"
+    sudo docker tag "$image:latest" "synth/$image:$SYNTH_RELEASE"
+    sudo docker push "$image:$SYNTH_RELEASE"
   fi
 done
