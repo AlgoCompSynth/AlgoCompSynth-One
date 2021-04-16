49526b1fe68a674f060d7d3e77636aef3b1c1578
M. Edward (Ed) Borasky <znmeb@algocompsynth.com>
2021-04-15 19:52:33 -0700

    Package xpra and then install

---
+++ docker-contexts/algocompsynth-1/package-xpra-html5.sh
@@ -0,0 +1,38 @@
+#! /bin/bash 
+
+# Copyright (C) 2021 M. Edward (Ed) Borasky <mailto:znmeb@algocompsynth.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+set -e
+rm -f $LOGS/xpra-html5.log
+
+pushd $SOURCE_DIR
+
+  echo "Cloning xpra-html5"
+  rm -fr xpra-html5
+  git clone https://github.com/Xpra-org/xpra-html5 \
+    >> $LOGS/xpra-html5.log 2>&1
+
+  echo "Checking out v$XPRA_HTML5_VERSION"
+  pushd xpra-html5
+    git checkout v$XPRA_HTML5_VERSION \
+      >> $LOGS/xpra-html5.log 2>&1
+
+    echo "Building packages"
+    python3 setup.py deb \
+      >> $LOGS/xpra-html5.log 2>&1
+    popd
+
+  popd
