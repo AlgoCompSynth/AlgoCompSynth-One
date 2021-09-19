3ad341a1ef77ac0311238969b8f2703d23595e5e
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-09-18 17:24:11 -0700

    cusignal build working

--- docker-contexts/synth/cusignal.sh
+++ docker-contexts/synth/cusignal.sh
@@ -20,39 +20,28 @@ rm -f $HOME/Logfiles/cusignal.log
 cd $HOME/Downloads/Installers
 
 echo "Cloning 'cusignal'"
-export CUSIGNAL_VERSION=v0.18.0
 export CUSIGNAL_HOME=$(pwd)/cusignal
 rm -fr $CUSIGNAL_HOME
 git clone https://github.com/rapidsai/cusignal.git $CUSIGNAL_HOME \
   >> $HOME/Logfiles/cusignal.log 2>&1
 cd $CUSIGNAL_HOME
-echo "Checking out version '$CUSIGNAL_VERSION'"
-git checkout $CUSIGNAL_VERSION \
+echo "Checking out version v$CUSIGNAL_VERSION"
+git checkout v$CUSIGNAL_VERSION \
   >> $HOME/Logfiles/cusignal.log 2>&1
 
-echo "Creating 'r-reticulate' conda environment"
+echo "Activating 'conda' executable"
 source $HOME/miniconda3/etc/profile.d/conda.sh
 
-# for compatibility with the R / reticulate conventions
-# we change the name of the conda environment
-sed --in-place=.bak --expression='s/cusignal-dev/r-reticulate/' \
-  conda/environments/cusignal_jetson_base.yml
+echo "Creating 'cusignal-dev' environment"
 /usr/bin/time conda env create \
   --file conda/environments/cusignal_jetson_base.yml \
   --quiet \
   >> $HOME/Logfiles/cusignal.log 2>&1
-conda activate r-reticulate
+conda activate cusignal-dev
 
 echo "Building 'cusignal'"
 /usr/bin/time ./build.sh --allgpuarch \
   >> $HOME/Logfiles/cusignal.log 2>&1
-
-echo "Installing 'JupyterLab'"
-/usr/bin/time conda install --quiet --yes jupyterlab \
-  >> $HOME/Logfiles/cusignal.log 2>&1
-
-echo "Enabling R kernel"
-Rscript -e "IRkernel::installspec()"
 
 echo "Copying '$CUSIGNAL_HOME/notebooks' to '$HOME/Notebooks/cusignal-notebooks'"
 mkdir --parents $HOME/Notebooks/
@@ -62,4 +51,4 @@ echo "Cleaning up"
 conda clean --tarballs --index-cache --quiet --yes
 conda list \
   >> $HOME/Logfiles/cusignal.log 2>&1
-rm -fr $CUSIGNAL_HOME
+#rm -fr $CUSIGNAL_HOME
