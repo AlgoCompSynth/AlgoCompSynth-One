752d0e8ceb122f869e900ed059525fc0d5413311
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-12-08 19:40:43 -0800

    move password to server config

--- docker-contexts/synth/start-jupyter-lab.sh
+++ docker-contexts/synth/start-jupyter-lab.sh
@@ -23,6 +23,6 @@ conda activate r-reticulate
 echo "Generating Jupyter configuration file"
 jupyter notebook --generate-config
 echo "Enter the same strong password twice"
-jupyter notebook password
+jupyter server password
 echo "If running remotely, browse to port 8888 on this Jetson host instead of 'localhost'"
 SHELL=/bin/bash jupyter lab --no-browser --ip=0.0.0.0
