e7b58497cf9121ee1cfd211a0d5dfc45449f8138
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-12-09 14:02:19 -0800

    remove existing container if any

--- host-scripts/20run-synth.sh
+++ host-scripts/20run-synth.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+echo "Removing 'synth' container"
+echo "Ignore 'No such container' errors"
+docker rm synth || true
+
 echo "Running 'algocompsynth/synth:latest'"
 docker run --interactive --tty \
   --name synth \
