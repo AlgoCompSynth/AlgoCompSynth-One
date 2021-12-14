19794e4a07a616be9174e60295a1add04cb058b9
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-12-13 20:18:25 -0800

    switching to mamba for virtual environment

--- docker-contexts/pytorch-builder/create-venv.sh
+++ docker-contexts/pytorch-builder/create-venv.sh
@@ -5,11 +5,10 @@ rm -f $LOGS/create-venv.log
 cd $SOURCE_DIR
 
 echo "Creating fresh 'gnash' conda environment"
-source $HOME/miniconda3/etc/profile.d/conda.sh
-conda create --force --quiet --yes --name gnash \
+source $HOME/mambaforge/etc/profile.d/mamba.sh
+mamba create --force --quiet --yes --name gnash \
   python=3.9 \
+  numpy=1.20.1 \
   >> $LOGS/create-venv.log 2>&1
-conda clean --all \
-  >> $LOGS/create-venv.log 2>&1
-conda list \
+mamba list \
   >> $LOGS/create-venv.log 2>&1
