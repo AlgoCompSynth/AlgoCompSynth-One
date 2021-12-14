52c3b9c96a20423ed613895bb99a9778065da7a3
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-12-13 22:44:40 -0800

    run the image builds niced

--- docker-contexts/all-images.sh
+++ docker-contexts/all-images.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-./image.sh pytorch-builder > /tmp/i-pytorch-builder.log 2>&1
-./image.sh synth > /tmp/i-synth.log 2>&1
+nice ./image.sh pytorch-builder > /tmp/i-pytorch-builder.log 2>&1
+nice ./image.sh synth > /tmp/i-synth.log 2>&1
