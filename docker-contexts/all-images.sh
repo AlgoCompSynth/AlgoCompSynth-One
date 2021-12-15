c5097fe432f0336cc1db3532d801ea58177f5652
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2021-12-15 10:29:26 -0800

    get rid of nice

--- docker-contexts/all-images.sh
+++ docker-contexts/all-images.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-nice ./image.sh pytorch-builder > /tmp/i-pytorch-builder.log 2>&1
-nice ./image.sh synth > /tmp/i-synth.log 2>&1
+./image.sh pytorch-builder > /tmp/i-pytorch-builder.log 2>&1
+./image.sh synth > /tmp/i-synth.log 2>&1
