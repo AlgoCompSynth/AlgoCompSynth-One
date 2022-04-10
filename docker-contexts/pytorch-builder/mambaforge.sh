3e6b1c0fc3fb092ff5540ce15eff3a91733396a3
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-04-10 16:17:59 -0700

    standardize mambaforge installer

--- docker-contexts/pytorch-builder/mambaforge.sh
+++ docker-contexts/pytorch-builder/mambaforge.sh
@@ -2,35 +2,33 @@
 
 set -e
 
-if [ -d "$HOME/mambaforge" ]
+if [ ! -d "$HOME/mambaforge" ]
 then
-  echo "$HOME/mambaforge exists - will not clobber!"
-  exit
+  echo "$HOME/mambaforge doesn't exist"
+  echo "Installing mambaforge"
+
+  export ARCH=`uname -m`
+  pushd /tmp
+
+    echo "Downloading latest Mambaforge installer"
+    rm -fr Mambaforge*
+    wget -q \
+      https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-$ARCH.sh
+    chmod +x Mambaforge-Linux-$ARCH.sh
+
+    echo "Installing Mambaforge to '$HOME/mambaforge' ..."
+    ./Mambaforge-Linux-$ARCH.sh -b
+
+    popd
 fi
 
-export ARCH=`uname -m`
-pushd /tmp
+echo "Updating base packages"
+source $HOME/mambaforge/etc/profile.d/conda.sh
+source $HOME/mambaforge/etc/profile.d/mamba.sh
+mamba update --name base --all --yes --quiet
 
-  echo "Downloading latest Mambaforge installer"
-  rm -fr Mambaforge*
-  wget -q \
-    https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-Linux-$ARCH.sh
-  chmod +x Mambaforge-Linux-$ARCH.sh
-
-  echo "Installing Mambaforge to '$HOME/mambaforge' ..."
-  ./Mambaforge-Linux-$ARCH.sh -b
-
-  echo "Updating base packages"
-  source $HOME/mambaforge/etc/profile.d/conda.sh
-  source $HOME/mambaforge/etc/profile.d/mamba.sh
-  mamba update --name base --all --yes --quiet
-
-  echo "Initializing 'bash' and 'zsh' command lines"
-  conda init bash
-  conda init zsh
-  mamba init bash
-  mamba init zsh
-  conda config --set auto_activate_base false
-  conda config --set default_threads 4
-
-  popd
+echo "Setting up bash and zsh command lines"
+mamba init bash
+mamba init zsh
+conda config --set auto_activate_base false
+conda config --set default_threads 4
