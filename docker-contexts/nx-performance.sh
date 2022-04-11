6d0bfe5df4782566ca1ad4a380e3dd57237b6530
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-04-11 16:12:08 -0700

    same comments as agx-performance

--- docker-contexts/nx-performance.sh
+++ docker-contexts/nx-performance.sh
@@ -2,9 +2,16 @@
 
 set -e
 
+echo "Before nvpmodel"
 sudo nvpmodel -q
 sudo nvpmodel -m 8 -d cool
+
+echo "After nvpmodel"
 sudo nvpmodel -q
+
+echo "Before jetson_clocks"
 sudo jetson_clocks --show
 sudo jetson_clocks --fan
+
+echo "After jetson_clocks"
 sudo jetson_clocks --show
