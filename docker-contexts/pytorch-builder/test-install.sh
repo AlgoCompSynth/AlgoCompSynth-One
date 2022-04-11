704937b99d27df5290e185445a89ab24106a1d98
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-04-11 02:43:51 -0700

    add torchaudio build to test at end

--- docker-contexts/pytorch-builder/test-install.sh
+++ docker-contexts/pytorch-builder/test-install.sh
@@ -9,7 +9,26 @@ source $HOME/mambaforge/etc/profile.d/conda.sh
 source $HOME/mambaforge/etc/profile.d/mamba.sh
 mamba activate pytorch-builder
 
+echo "Installing PyTorch wheel"
 pip install $PACKAGES/torch*.whl \
   >> $SYNTH_LOGS/test-install.log 2>&1
+
+echo "Cloning 'torchaudio'"
+rm -fr audio*
+git clone --recurse-submodules https://github.com/pytorch/audio.git \
+  >> $SYNTH_LOGS/test-install.log 2>&1
+cd audio
+git checkout v$TORCHAUDIO_VERSION \
+  >> $SYNTH_LOGS/test-install.log 2>&1
+
+echo "Installing 'torchaudio'"
+/usr/bin/time python setup.py install \
+  >> $SYNTH_LOGS/test-install.log 2>&1
+popd
+
+echo "Cleanup"
 mamba list \
   >> $SYNTH_LOGS/test-install.log 2>&1
+mamba clean  --all --yes \
+  >> $SYNTH_LOGS/test-install.log 2>&1
+rm -fr /tmp/audio
