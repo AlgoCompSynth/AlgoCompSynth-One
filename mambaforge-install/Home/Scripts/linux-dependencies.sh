1b72e6053373432f2d23efe48306b948d41663c5
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-04-15 14:01:49 -0700

    add PyTorch Linux dependencies

--- mambaforge-install/Home/Scripts/linux-dependencies.sh
+++ mambaforge-install/Home/Scripts/linux-dependencies.sh
@@ -23,6 +23,9 @@ sudo apt-get upgrade -y
 sudo apt-get install -qqy --no-install-recommends \
   ffmpeg \
   flac \
+  libopenblas-base \
+  libopenmpi-dev \
+  libomp-dev \
   libsox-dev \
   libsoxr-dev \
   libsox-fmt-all \
