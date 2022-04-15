1a0a01851b600924e6baf0cfcb92d3de976db634
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-04-14 17:33:48 -0700

    performance setter for NX on JetPack 5

---
+++ docker-contexts/nx-jp5-performance.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+set -e
+
+echo "Before nvpmodel"
+sudo nvpmodel --query
+sudo nvpmodel --mode 8
+
+echo "After nvpmodel"
+sudo nvpmodel --query
+
+echo "Before jetson_clocks"
+sudo jetson_clocks --show
+sudo jetson_clocks --fan
+
+echo "After jetson_clocks"
+sudo jetson_clocks --show
