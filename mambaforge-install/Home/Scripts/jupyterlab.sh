03963b74874f2152d52a6461bda887767da955fe
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-04-16 15:46:54 -0700

    need jupyterlab-git

--- mambaforge-install/Home/Scripts/jupyterlab.sh
+++ mambaforge-install/Home/Scripts/jupyterlab.sh
@@ -9,9 +9,12 @@ mamba activate r-reticulate
 echo "Installing mamba dependencies"
 # jupyter_server<1.13.2 workaround
 # https://github.com/jupyterlab/jupyterlab/issues/12094
+# need jupyterlab-git for git
+# https://github.com/jupyterlab/jupyterlab-git/issues/419
 mamba install --quiet --yes \
   "jupyter_server<1.13.2" \
-  jupyterlab
+  jupyterlab \
+  jupyterlab-git
 
 echo "Cleanup"
 mamba list
