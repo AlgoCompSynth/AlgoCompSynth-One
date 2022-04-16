a7200a931b782191a212b596f7e72716575fe1b2
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-04-15 17:19:51 -0700

    Pin r-base to 4.0.2

--- mambaforge-install/Home/Scripts/jupyterlab.sh
+++ mambaforge-install/Home/Scripts/jupyterlab.sh
@@ -23,12 +23,7 @@ mamba activate r-reticulate
 
 echo "Installing mamba dependencies"
 mamba install --quiet --yes \
-  libgit2 \
-  r-base
-
-echo "Installing R packages"
-export PKG_CONFIG_PATH=$CONDA_PREFIX/lib/pkgconfig
-/usr/bin/time $SYNTH_SCRIPTS/devtools.R
+  jupyterlab
 
 echo "Cleanup"
 mamba list
