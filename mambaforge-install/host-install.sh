b00672549aafb79e0c3d1286593cd992e4ae7437
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-04-16 19:46:32 -0700

    clean up script names

--- mambaforge-install/host-install.sh
+++ mambaforge-install/host-install.sh
@@ -7,34 +7,44 @@ source 00envars
 
 echo "Creating fresh $SYNTH_HOME"
 rm -fr $SYNTH_HOME
-cp -rp Home $SYNTH_HOME
+mkdir --parents \
+  $SYNTH_SCRIPTS \
+  $SYNTH_LOGS \
+  $SYNTH_NOTEBOOKS \
+  $SYNTH_PROJECTS
 
 echo "Installing Linux dependencies"
+cp Home/Scripts/linux-dependencies.sh $SYNTH_SCRIPTS/
 $SYNTH_SCRIPTS/linux-dependencies.sh > $SYNTH_LOGS/linux-dependencies.log 2>&1
 
 echo "Installing Mambaforge if needed"
+cp Home/Scripts/mambaforge.sh $SYNTH_SCRIPTS/
 $SYNTH_SCRIPTS/mambaforge.sh > $SYNTH_LOGS/mambaforge.log 2>&1
 
 echo "Installing cusignal in fresh mamba environment 'r-reticulate'"
-sed "s/PYTHON_VERSION/$PYTHON_VERSION/" $SYNTH_SCRIPTS/cusignal_jetson_base_template \
-  > $SYNTH_ENV_FILE
+cp Home/Scripts/cusignal*.sh $SYNTH_SCRIPTS/
 /usr/bin/time $SYNTH_SCRIPTS/cusignal.sh > $SYNTH_LOGS/cusignal.log 2>&1
 
 echo "Installing PyTorch"
+cp Home/Scripts/*pytorch* $SYNTH_SCRIPTS/
 /usr/bin/time $SYNTH_SCRIPTS/pytorch.sh > $SYNTH_LOGS/pytorch.log 2>&1
 $SYNTH_SCRIPTS/test-pytorch.sh
 
 echo "Installing torchaudio"
-/usr/bin/time $SYNTH_SCRIPTS/torchaudio.sh #> $SYNTH_LOGS/torchaudio.log 2>&1
+cp Home/Scripts/*torchaudio* $SYNTH_SCRIPTS/
+/usr/bin/time $SYNTH_SCRIPTS/torchaudio.sh > $SYNTH_LOGS/torchaudio.log 2>&1
 $SYNTH_SCRIPTS/test-torchaudio.sh
 
 echo "Installing R developer tools"
+cp Home/Scripts/*devtools* $SYNTH_SCRIPTS/
 /usr/bin/time $SYNTH_SCRIPTS/R-devtools.sh > $SYNTH_LOGS/R-devtools.log 2>&1
 
-echo "Installing R audio packages"
-/usr/bin/time $SYNTH_SCRIPTS/R-audio.sh > $SYNTH_LOGS/R-audio.log 2>&1
+echo "Installing R sound packages"
+cp Home/Scripts/*sound* $SYNTH_SCRIPTS/
+/usr/bin/time $SYNTH_SCRIPTS/R-sound.sh > $SYNTH_LOGS/R-sound.log 2>&1
 
 echo "Installing JupyterLab"
+cp Home/Scripts/jupyterlab.sh $SYNTH_SCRIPTS/
 /usr/bin/time $SYNTH_SCRIPTS/jupyterlab.sh > $SYNTH_LOGS/jupyterlab.log 2>&1
 
 echo "Finished!"
