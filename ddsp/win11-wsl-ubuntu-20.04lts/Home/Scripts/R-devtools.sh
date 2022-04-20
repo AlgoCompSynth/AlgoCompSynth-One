bf9971a6c36a1eda74155b969ad67c885e8c099d
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-04-20 12:50:37 -0700

    need libgit2 for devtools

--- ddsp/win11-wsl-ubuntu-20.04lts/Home/Scripts/R-devtools.sh
+++ ddsp/win11-wsl-ubuntu-20.04lts/Home/Scripts/R-devtools.sh
@@ -6,11 +6,9 @@ source $HOME/mambaforge/etc/profile.d/conda.sh
 source $HOME/mambaforge/etc/profile.d/mamba.sh
 mamba activate r-reticulate
 
-#echo "Installing mamba dependencies"
-#mamba install --quiet --yes \
-  #libgit2 \
-  #pandoc \
-  #r-base
+echo "Installing mamba dependencies"
+mamba install --quiet --yes \
+  libgit2
 
 echo "Installing R packages"
 export PKG_CONFIG_PATH=$CONDA_PREFIX/lib/pkgconfig
