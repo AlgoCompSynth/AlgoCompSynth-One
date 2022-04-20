ccfe97d8b2cf91e62c62ac6659282b41fef3f35b
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-04-19 18:59:34 -0700

    use symlinks for build and run scripts

--- ddsp/jetson-xavier-jetpack-4.6.1/docker-build.sh
+++ ddsp/jetson-xavier-jetpack-4.6.1/docker-build.sh
@@ -1,12 +1,1 @@
-#! /bin/bash
-
-set -e
-
-export REGISTRY="docker.io"
-export ACCOUNT="algocompsynth"
-export REPO="ddsp"
-
-echo "Creating a backup - ignore missing images"
-docker tag $REGISTRY/$ACCOUNT/$REPO:latest $REGISTRY/$ACCOUNT/$REPO:backup || true
-echo "Building $REPO"
-/usr/bin/time docker build --tag $REGISTRY/$ACCOUNT/$REPO:latest . > /tmp/$REPO.log 2>&1
+../docker-build.sh
--- ddsp/win11-wsl-ubuntu-20.04lts/docker-run.sh
+++ ddsp/win11-wsl-ubuntu-20.04lts/docker-run.sh
@@ -1,14 +1,1 @@
-#! /bin/bash
-
-set -e
-
-echo "Removing 'ddsp' container"
-echo "Ignore 'No such container' errors"
-docker rm ddsp || true
-
-echo "Running 'algocompsynth/ddsp:latest'"
-docker run --interactive --tty \
-  --name ddsp \
-  --network host \
-  --runtime nvidia \
-  algocompsynth/ddsp:latest /bin/bash
+../docker-run.sh
