b51f41ddf390612616f01fb8b7764aa04797988b
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-04-19 21:08:43 -0700

    moar cleanup

--- ddsp/win11-wsl-ubuntu-20.04lts/Home/Scripts/jupyterlab.sh
+++ ddsp/win11-wsl-ubuntu-20.04lts/Home/Scripts/jupyterlab.sh
@@ -4,11 +4,10 @@ set -e
 
 source $HOME/mambaforge/etc/profile.d/conda.sh
 source $HOME/mambaforge/etc/profile.d/mamba.sh
-
-echo "Creating fresh r-reticulate virtual environment"
-sed -i.bak "s/PYTHON_VERSION/$PYTHON_VERSION/" $SYNTH_SCRIPTS/r-reticulate.yml
-/usr/bin/time mamba env create --force --file $SYNTH_SCRIPTS/r-reticulate.yml
 mamba activate r-reticulate
+
+echo "Enabling R kernel"
+Rscript -e "IRkernel::installspec()"
 
 echo "Cleanup"
 mamba list
