031a637f15f1e250c5df3d1c8e6bf2c384250e69
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-04-20 12:53:13 -0700

    add comment about chromium-browser

--- ddsp/win11-wsl-ubuntu-20.04lts/Home/Scripts/linux-dependencies.sh
+++ ddsp/win11-wsl-ubuntu-20.04lts/Home/Scripts/linux-dependencies.sh
@@ -5,6 +5,8 @@ set -e
 echo "Installing Linux dependencies"
 sudo apt-get update
 sudo apt-get upgrade -y
+
+# we use chromium-browser because it's the default on Jetsons
 sudo apt-get install -qqy --no-install-recommends \
   chromium-browser \
   ffmpeg \
