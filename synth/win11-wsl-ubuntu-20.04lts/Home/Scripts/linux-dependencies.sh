2648613e39a491622fae9ef6c9c93a38f6fb54e6
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-04-20 18:29:57 -0700

    chromium-browser doesn't work - use firefox

--- synth/win11-wsl-ubuntu-20.04lts/Home/Scripts/linux-dependencies.sh
+++ synth/win11-wsl-ubuntu-20.04lts/Home/Scripts/linux-dependencies.sh
@@ -6,10 +6,9 @@ echo "Installing Linux dependencies"
 sudo apt-get update
 sudo apt-get upgrade -y
 
-# we use chromium-browser because it's the default on Jetsons
 sudo apt-get install -qqy --no-install-recommends \
   bash-completion \
-  chromium-browser \
+  firefox \
   ffmpeg \
   flac \
   less \
