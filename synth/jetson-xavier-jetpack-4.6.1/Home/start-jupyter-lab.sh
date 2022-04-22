44a21e340296f1af314659bec3a324941ac54bef
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-04-21 23:37:41 -0700

    sync with WSL version

--- synth/jetson-xavier-jetpack-4.6.1/Home/start-jupyter-lab.sh
+++ synth/jetson-xavier-jetpack-4.6.1/Home/start-jupyter-lab.sh
@@ -12,5 +12,5 @@ echo "Generating Jupyter configuration file"
 jupyter notebook --generate-config
 echo "Enter the same strong password twice"
 jupyter server password
-echo "If running remotely, browse to port 8888 on this Jetson host instead of 'localhost'"
+echo "If running remotely, browse to port 8888 on this host instead of 'localhost'"
 SHELL=/bin/bash jupyter lab --no-browser --ip=0.0.0.0
