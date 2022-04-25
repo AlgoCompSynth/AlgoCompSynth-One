c85e538fba955df1b6dc40f6d2b469e385d03a71
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-04-25 14:53:30 -0700

    fix typo

--- wheel-builder/jetpack-5.0DP/Home/Scripts/copy-cached-wheels.sh
+++ wheel-builder/jetpack-5.0DP/Home/Scripts/copy-cached-wheels.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Saving cached wheels"
-for wheel in `find $HOME/.cache/pip/wheels -name "*.whl"
+for wheel in `find $HOME/.cache/pip/wheels -name "*.whl"`
 do
   echo $wheel
   cp $wheel $SYNTH_WHEELS/
