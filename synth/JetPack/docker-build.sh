fd7a633bf504c86dce9c90197bd5975db01b144f
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-04-27 15:37:13 -0700

    workaround for Docker parameter passing

--- synth/JetPack/docker-build.sh
+++ synth/JetPack/docker-build.sh
@@ -5,7 +5,7 @@ set -e
 export REGISTRY="docker.io"
 export ACCOUNT="algocompsynth"
 export REPO="synth"
-export FROM_REPO="wheel-builder"
+export WHEEL_REPO="wheel-builder"
 
 echo "Detecting JetPack version"
 export PATH=$PATH:/usr/local/cuda/bin
@@ -16,14 +16,14 @@ then
   echo "JetPack 5 detected"
   export BASE_IMAGE="nvcr.io/nvidia/l4t-base:r34.1"
   export IMAGE_TAG=`echo $BASE_IMAGE | sed "s/^.*://"`
-  export FROM_IMAGE="$REGISTRY/$ACCOUNT/$FROM_REPO:$IMAGE_TAG"
+  export WHEEL_IMAGE="$REGISTRY/$ACCOUNT/$WHEEL_REPO:$IMAGE_TAG"
   export PYTHON_VERSION="3.8"
 elif [ "$JETPACK4" -gt "0" ]
 then
   echo "JetPack 4 detected"
   export BASE_IMAGE="nvcr.io/nvidia/l4t-base:r32.7.1"
   export IMAGE_TAG=`echo $BASE_IMAGE | sed "s/^.*://"`
-  export FROM_IMAGE="$REGISTRY/$ACCOUNT/$FROM_REPO:$IMAGE_TAG"
+  export WHEEL_IMAGE="$REGISTRY/$ACCOUNT/$WHEEL_REPO:$IMAGE_TAG"
   export PYTHON_VERSION="3.6"
 else
   echo "ERROR: can't detect JetPack version!"
@@ -33,10 +33,13 @@ fi
 echo "Pulling $BASE_IMAGE"
 docker pull $BASE_IMAGE
 
+echo "Editing Dockerfile"
+sed --expression="s;--from=WHEEL_IMAGE;--from=$WHEEL_IMAGE;" Dockerfile.template > Dockerfile
+
 echo "Building $REPO"
 /usr/bin/time docker build \
   --build-arg "ARG_BASE_IMAGE=$BASE_IMAGE" \
-  --build-arg "ARG_FROM_IMAGE=$FROM_IMAGE" \
+  --build-arg "ARG_WHEEL_IMAGE=$WHEEL_IMAGE" \
   --build-arg "ARG_PYTHON_VERSION=$PYTHON_VERSION" \
   --tag $REGISTRY/$ACCOUNT/$REPO:$IMAGE_TAG \
   . > /tmp/$REPO.log 2>&1
