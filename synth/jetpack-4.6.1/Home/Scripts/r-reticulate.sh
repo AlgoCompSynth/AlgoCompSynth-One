ef97d2be2a932970343a88eb72e4c91a94705015
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-04-26 20:12:49 -0700

    eliminate "typing-extensions" hack

--- synth/jetpack-4.6.1/Home/Scripts/r-reticulate.sh
+++ synth/jetpack-4.6.1/Home/Scripts/r-reticulate.sh
@@ -7,7 +7,6 @@ source $HOME/mambaforge/etc/profile.d/mamba.sh
 
 echo "Creating fresh r-reticulate virtual environment"
 sed -i.bak "s/PYTHON_VERSION/$PYTHON_VERSION/" $SYNTH_SCRIPTS/r-reticulate.yml
-sed -i "s/TYPING_EXTENSIONS_VERSION/$TYPING_EXTENSIONS_VERSION/" $SYNTH_SCRIPTS/r-reticulate.yml
 /usr/bin/time mamba env create --force --file $SYNTH_SCRIPTS/r-reticulate.yml
 
 echo "Cleanup"
