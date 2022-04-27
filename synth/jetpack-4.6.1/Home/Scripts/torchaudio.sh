0f026f9478f409221b301267b2ef925fd88b0190
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-04-26 23:00:48 -0700

    patch for torchaudio download failure

--- synth/jetpack-4.6.1/Home/Scripts/torchaudio.sh
+++ synth/jetpack-4.6.1/Home/Scripts/torchaudio.sh
@@ -21,6 +21,21 @@ cd audio
 echo "Checking out v$TORCHAUDIO_VERSION"
 git checkout v$TORCHAUDIO_VERSION
 
+echo "Patching source"
+if [ "$TORCHAUDIO_VERSION" -eq "0.11.0" ]
+then
+
+# https://github.com/dusty-nv/jetson-containers/blob/master/Dockerfile.pytorch
+# https://github.com/pytorch/audio/issues/2295
+sed -i \
+  's#  URL https://zlib.net/zlib-1.2.11.tar.gz#  URL https://zlib.net/zlib-1.2.12.tar.gz#g' \
+  third_party/zlib/CMakeLists.txt || echo "failed to patch torchaudio/third_party/zlib/CMakeLists.txt"
+sed -i \
+  's#  URL_HASH SHA256=c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1#  URL_HASH SHA256=91844808532e5ce316b3c010929493c0244f3d37593afd6de04f71821d5136d9#g' \
+  third_party/zlib/CMakeLists.txt || echo "failed to patch torchaudio/third_party/zlib/CMakeLists.txt"
+
+fi
+
 echo "Building torchaudio wheel"
 export BUILD_SOX=1
 /usr/bin/time python setup.py bdist_wheel
