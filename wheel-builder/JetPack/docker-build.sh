e195c76da25cd239eddae50021ce12b2d9b922b2
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-04-27 17:40:07 -0700

    switch to L4T CUDA base

--- wheel-builder/JetPack/docker-build.sh
+++ wheel-builder/JetPack/docker-build.sh
@@ -13,7 +13,7 @@ export JETPACK5=`nvcc --version | grep -e "11.4" | wc -l`
 if [ "$JETPACK5" -gt "0" ]
 then
   echo "JetPack 5 detected"
-  export BASE_IMAGE="nvcr.io/nvidia/l4t-base:r34.1"
+  export BASE_IMAGE="nvcr.io/nvidia/l4t-cuda:11.4.14-runtime"
   export IMAGE_TAG=`echo $BASE_IMAGE | sed "s/^.*://"`
   export PYTHON_VERSION="3.8"
   export PYTORCH_WHEEL_URL="https://developer.download.nvidia.com/compute/redist/jp/v50/pytorch/torch-1.12.0a0+2c916ef.nv22.3-cp38-cp38-linux_aarch64.whl"
@@ -22,7 +22,7 @@ then
 elif [ "$JETPACK4" -gt "0" ]
 then
   echo "JetPack 4 detected"
-  export BASE_IMAGE="nvcr.io/nvidia/l4t-base:r32.7.1"
+  export BASE_IMAGE="nvcr.io/nvidia/l4t-cuda:10.2.460-runtime"
   export IMAGE_TAG=`echo $BASE_IMAGE | sed "s/^.*://"`
   export PYTHON_VERSION="3.6"
   export PYTORCH_WHEEL_URL="https://nvidia.box.com/shared/static/fjtbno0vpo676a25cgvuqc1wty0fkkg6.whl"
