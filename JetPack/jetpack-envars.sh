bc011fe24088140e1d2ef42fd1bb21c6a51396b2
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-04-29 13:22:08 -0700

    standardize naming a bit

--- JetPack/jetpack-envars.sh
+++ JetPack/jetpack-envars.sh
@@ -1,6 +1,6 @@
 export REGISTRY="docker.io"
 export ACCOUNT="algocompsynth"
-export WHEEL_REPO="wheel-builder"
+export WHEEL_REPO="wheel"
 export SYNTH_REPO="synth"
 
 echo "Detecting JetPack version"
@@ -11,6 +11,7 @@ if [ "$JETPACK5" -gt "0" ]
 then
   echo "JetPack 5 detected"
   export BASE_IMAGE="nvcr.io/nvidia/l4t-base:r34.1"
+  export IMAGE_TAG="jp5"
   export PYTHON_VERSION="3.8"
   export PYTORCH_WHEEL_URL="https://developer.download.nvidia.com/compute/redist/jp/v50/pytorch/torch-1.12.0a0+2c916ef.nv22.3-cp38-cp38-linux_aarch64.whl"
   export PYTORCH_WHEEL_FILE="torch-1.12.0a0+2c916ef.nv22.3-cp38-cp38-linux_aarch64.whl"
@@ -19,7 +20,7 @@ elif [ "$JETPACK4" -gt "0" ]
 then
   echo "JetPack 4 detected"
   export BASE_IMAGE="nvcr.io/nvidia/l4t-base:r32.7.1"
-  export IMAGE_TAG=`echo $BASE_IMAGE | sed "s/^.*://"`
+  export IMAGE_TAG="jp4"
   export PYTHON_VERSION="3.6"
   export PYTORCH_WHEEL_URL="https://nvidia.box.com/shared/static/fjtbno0vpo676a25cgvuqc1wty0fkkg6.whl"
   export PYTORCH_WHEEL_FILE="torch-1.10.0-cp36-cp36m-linux_aarch64.whl"
@@ -30,6 +31,5 @@ else
 fi
 
 echo "Defining common envars"
-export IMAGE_TAG=`echo $BASE_IMAGE | sed "s/^.*://"`
 export WHEEL_IMAGE="$REGISTRY/$ACCOUNT/$WHEEL_REPO:$IMAGE_TAG"
 export SYNTH_IMAGE="$REGISTRY/$ACCOUNT/$SYNTH_REPO:$IMAGE_TAG"
