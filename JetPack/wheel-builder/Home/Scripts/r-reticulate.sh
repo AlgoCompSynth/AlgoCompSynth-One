41d5050ccc9d07d3ea3c4c6df957c44ff657db76
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-04-28 21:03:43 -0700

    configure widgets

--- JetPack/wheel-builder/Home/Scripts/r-reticulate.sh
+++ JetPack/wheel-builder/Home/Scripts/r-reticulate.sh
@@ -9,8 +9,14 @@ echo "Creating fresh r-reticulate virtual environment"
 sed "s/PYTHON_VERSION/$PYTHON_VERSION/" $SYNTH_SCRIPTS/r-reticulate.template > $SYNTH_SCRIPTS/r-reticulate.yml
 /usr/bin/time mamba env create --force --file $SYNTH_SCRIPTS/r-reticulate.yml
 
-echo "Cleanup"
+echo "Activating r-reticulate"
 mamba activate r-reticulate
+
+echo "Configuring widgets"
+jupyter nbextension install --py widgetsnbextension --user
+jupyter nbextension enable --py widgetsnbextension
+
+echo "Cleanup"
 mamba list
 mamba clean --tarballs --yes
 
