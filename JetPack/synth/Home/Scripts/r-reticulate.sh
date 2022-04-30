9b0348fe090d7e7bed4fd1589656795da192b0b1
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-04-29 17:16:40 -0700

    clean up widget install

--- JetPack/synth/Home/Scripts/r-reticulate.sh
+++ JetPack/synth/Home/Scripts/r-reticulate.sh
@@ -13,8 +13,8 @@ echo "Activating r-reticulate"
 mamba activate r-reticulate
 
 echo "Configuring widgets"
-jupyter nbextension install --py widgetsnbextension --sys-prefix
-jupyter nbextension enable --py widgetsnbextension
+jupyter nbextension install widgetsnbextension --py --sys-prefix
+jupyter nbextension enable widgetsnbextension --py --sys-prefix
 
 echo "Cleanup"
 mamba list
