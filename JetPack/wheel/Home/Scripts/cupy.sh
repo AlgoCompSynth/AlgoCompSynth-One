bb7661ddf00d8ccc03ac8b86aa9579594bf792ce
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-04-30 10:14:45 -0700

    pip needs pinned package names quoted

--- JetPack/wheel/Home/Scripts/cupy.sh
+++ JetPack/wheel/Home/Scripts/cupy.sh
@@ -9,7 +9,7 @@ export PATH=$PATH:/usr/local/cuda/bin
 echo "PATH is now $PATH"
 
 echo "Installing CuPy - this takes a long time!"
-/usr/bin/time pip install cupy>=9.0.0
+/usr/bin/time pip install 'cupy>=9.0.0'
 
 echo "Cleanup"
 mamba list
