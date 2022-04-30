b58cb56199af3c626cd9e0ff901df37c6ee8e58d
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-04-29 17:14:55 -0700

    pass all envars to build

--- JetPack/synth/docker-build.sh
+++ JetPack/synth/docker-build.sh
@@ -15,7 +15,10 @@ sed "s;--from=WHEEL_IMAGE;--from=$WHEEL_IMAGE;" Dockerfile.template > Dockerfile
 echo "Building $REPO"
 /usr/bin/time docker build \
   --build-arg "ARG_BASE_IMAGE=$BASE_IMAGE" \
-  --build-arg "ARG_WHEEL_IMAGE=$WHEEL_IMAGE" \
   --build-arg "ARG_PYTHON_VERSION=$PYTHON_VERSION" \
+  --build-arg "ARG_PYTORCH_WHEEL_URL=$PYTORCH_WHEEL_URL" \
+  --build-arg "ARG_PYTORCH_WHEEL_FILE=$PYTORCH_WHEEL_FILE" \
+  --build-arg "ARG_TORCHAUDIO_VERSION=$TORCHAUDIO_VERSION" \
+  --build-arg "ARG_WHEEL_IMAGE=$WHEEL_IMAGE" \
   --tag $REGISTRY/$ACCOUNT/$REPO:$IMAGE_TAG \
   . > /tmp/$REPO.log 2>&1
