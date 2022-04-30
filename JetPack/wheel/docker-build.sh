17744946261046661318fccbadfe7aefd0a10fe2
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-04-29 22:55:29 -0700

    need to pass IMAGE_TAG

--- JetPack/wheel/docker-build.sh
+++ JetPack/wheel/docker-build.sh
@@ -13,10 +13,11 @@ docker pull $BASE_IMAGE
 echo "Building $REPO"
 /usr/bin/time docker build \
   --build-arg "ARG_BASE_IMAGE=$BASE_IMAGE" \
+  --build-arg "ARG_CUDA_INSTALL=$CUDA_INSTALL" \
+  --build-arg "ARG_IMAGE_TAG=$IMAGE_TAG" \
   --build-arg "ARG_PYTHON_VERSION=$PYTHON_VERSION" \
   --build-arg "ARG_PYTORCH_WHEEL_URL=$PYTORCH_WHEEL_URL" \
   --build-arg "ARG_PYTORCH_WHEEL_FILE=$PYTORCH_WHEEL_FILE" \
   --build-arg "ARG_TORCHAUDIO_VERSION=$TORCHAUDIO_VERSION" \
-  --build-arg "ARG_CUDA_INSTALL=$CUDA_INSTALL" \
   --tag $REGISTRY/$ACCOUNT/$REPO:$IMAGE_TAG \
   . > /tmp/$REPO.log 2>&1
