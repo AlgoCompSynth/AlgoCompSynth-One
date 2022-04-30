6483e57d4a399204f8a866ac15ca664ed754c81b
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-04-30 10:03:04 -0700

    new Python version logic

--- JetPack/synth/Home/Scripts/r-reticulate.sh
+++ JetPack/synth/Home/Scripts/r-reticulate.sh
@@ -6,7 +6,7 @@ source $HOME/mambaforge/etc/profile.d/conda.sh
 source $HOME/mambaforge/etc/profile.d/mamba.sh
 
 echo "Creating fresh r-reticulate virtual environment"
-sed "s/PYTHON_VERSION/$PYTHON_VERSION/" $SYNTH_SCRIPTS/r-reticulate.template > $SYNTH_SCRIPTS/r-reticulate.yml
+cp $SYNTH_SCRIPTS/r-reticulate.template.$PYTHON_VERSION $SYNTH_SCRIPTS/r-reticulate.yml
 /usr/bin/time mamba env create --force --file $SYNTH_SCRIPTS/r-reticulate.yml
 
 echo "Activating r-reticulate"
