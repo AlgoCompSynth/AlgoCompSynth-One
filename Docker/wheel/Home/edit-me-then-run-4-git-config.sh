90c36cc83aea362e05404c7ca8243244d34000b8
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-05-02 22:29:46 -0700

    Docker version checkpointed and removed

--- Docker/wheel/Home/edit-me-then-run-4-git-config.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-echo "Edit before running!"
-git config --global user.email ""
-git config --global user.name ""
-
-echo "Time in seconds to cache your credentials"
-git config --global credential.helper 'cache --timeout=7200'
