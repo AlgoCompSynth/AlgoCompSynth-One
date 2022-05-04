e0c232c40bd5deff306081f5bacbba9cd01b7c08
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-05-04 16:07:52 -0700

    build CuPy first

---
+++ Host/Scripts/cupy.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+set -e
+
+echo "Activating r-reticulate"
+source $HOME/mambaforge/etc/profile.d/conda.sh
+source $HOME/mambaforge/etc/profile.d/mamba.sh
+mamba activate r-reticulate
+export PATH=$PATH:/usr/local/cuda/bin
+
+echo "Installing CuPy - this can take some time if it's not cached!"
+pip install --verbose cupy>=9.0.0
+
+echo "Cleanup"
+mamba list
+mamba clean --tarballs --yes
+
+echo "Finished"
