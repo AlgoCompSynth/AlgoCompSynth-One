b6b326d5078218043b2117535a33b26f6a88d429
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2022-05-05 22:33:05 -0700

    switched to Mambaforge R!

---
+++ Host/Scripts/R-mambaforge.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+set -e
+
+echo "Activating r-reticulate"
+source $HOME/mambaforge/etc/profile.d/conda.sh
+source $HOME/mambaforge/etc/profile.d/mamba.sh
+mamba activate r-reticulate
+
+echo "Installing r-base"
+mamba install --quiet --yes \
+  r-base \
+  r-recommended
+
+echo "Cleanup"
+mamba list
+mamba clean --tarballs --yes
+
+echo "Finished"
