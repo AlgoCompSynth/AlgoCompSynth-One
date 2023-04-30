6a891fae6895f8ebf213c320c46a272cc839eaeb
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2023-04-29 19:46:42 -0700

    new build process

--- JetPack5/Scripts/torchaudio.sh
+++ JetPack5/Scripts/torchaudio.sh
@@ -11,6 +11,7 @@ export PATH=$PATH:/usr/local/cuda/bin
 echo "Installing mamba dependencies"
 mamba install --quiet --yes \
   cmake \
+  ffmpeg \
   ninja \
   pkg-config
 
@@ -25,32 +26,14 @@ then
   cd audio
   echo "Checking out v$TORCHAUDIO_VERSION"
   git checkout v$TORCHAUDIO_VERSION
-
-  echo "Patching source"
-  if [ "$TORCHAUDIO_VERSION" == "0.11.0" ]
-  then
-
-    # https://github.com/dusty-nv/jetson-containers/blob/master/Dockerfile.pytorch
-    # https://github.com/pytorch/audio/issues/2295
-    sed -i \
-      's#  URL https://zlib.net/zlib-1.2.11.tar.gz#  URL https://zlib.net/zlib-1.2.12.tar.gz#g' \
-      third_party/zlib/CMakeLists.txt || echo "failed to patch torchaudio/third_party/zlib/CMakeLists.txt"
-    sed -i \
-      's#  URL_HASH SHA256=c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1#  URL_HASH SHA256=91844808532e5ce316b3c010929493c0244f3d37593afd6de04f71821d5136d9#g' \
-      third_party/zlib/CMakeLists.txt || echo "failed to patch torchaudio/third_party/zlib/CMakeLists.txt"
-
-  fi
-
-  export BUILD_SOX=1
-  /usr/bin/time python setup.py bdist_wheel
-
+  echo "Installing from source"
+  /usr/bin/time USE_FFMPEG=1 python setup.py develop
   echo "Saving torchaudio wheel"
   cp dist/torchaudio-*.whl $SYNTH_WHEELS/
-
+else
+  echo "Installing torchaudio wheel"
+  pip install $SYNTH_WHEELS/torchaudio-*.whl
 fi
-
-echo "Installing torchaudio wheel"
-pip install $SYNTH_WHEELS/torchaudio-*.whl
 
 echo "Cleanup"
 echo "..Removing audio project repository"
