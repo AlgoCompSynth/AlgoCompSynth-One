36f5a1d29e2550bf53ef6e9b0ec071b927bdb004
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2023-05-01 11:23:25 -0700

    clean up CuPy build

--- JetPack5/Scripts/pytorch.sh
+++ JetPack5/Scripts/pytorch.sh
@@ -7,14 +7,10 @@ source $MAMBAFORGE_HOME/etc/profile.d/mamba.sh
 mamba activate r-reticulate
 export PATH=$PATH:/usr/local/cuda/bin
 
-echo "Installing Cython"
-mamba install --quiet --yes \
-  Cython
-
 pushd $SYNTH_WHEELS
 rm -f $PYTORCH_WHEEL_FILE
 echo "Downloading PyTorch wheel"
-wget --verbose $PYTORCH_WHEEL_URL --output-document=$PYTORCH_WHEEL_FILE
+wget --quiet $PYTORCH_WHEEL_URL --output-document=$PYTORCH_WHEEL_FILE
 popd
 
 echo "Installing PyTorch"
