2b6365f66e71deaf9ebdc7d2dc7b10a3a2abb138
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-05-04 16:09:56 -0700

    optional command-line setup

---
+++ Arch-Distrobox/22bash_zsh_setup.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/env bash
+
+set -e
+
+echo "Installing command line conveniences"
+sudo pacman --sync --refresh --needed --noconfirm \
+  python-pygments \
+  zsh \
+  zsh-autosuggestions \
+  zsh-completions \
+  zshdb \
+  zsh-doc \
+  zsh-history-substring-search \
+  zsh-lovers \
+  zsh-syntax-highlighting
+yay --sync --refresh --needed \
+  ttf-meslo-nerd-font-powerlevel10k \
+  zsh-theme-powerlevel10k
+
+echo "Setting shell control files"
+cp bashrc $HOME/.bashrc
+cp bash_aliases $HOME/.bash_aliases
+cp vimrc-dark $HOME/.vimrc
+sudo cp vimrc-dark /root/.vimrc
+cp zshrc $HOME/.zshrc
+cp p10k.zsh $HOME/.p10k.zsh
+
+echo "Setting default shell for $USER to zsh"
+sudo usermod --shell /usr/bin/zsh $USER
+
+echo "Creating $HOME/.local/bin and $HOME/bin"
+mkdir --parents $HOME/.local/bin
+mkdir --parents $HOME/bin
+
+echo "Finished"
