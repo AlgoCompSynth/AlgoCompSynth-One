e3d901498d6c4a017ec13d1621208c55c773b117
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-05-04 16:05:03 -0700

    move the shell setup to optional script

--- Arch-Distrobox/20initialize_arch.sh
+++ Arch-Distrobox/20initialize_arch.sh
@@ -31,33 +31,4 @@ echo "Updating package search databases"
 sudo pacman -Fy
 yay -Fy
 
-echo "Installing command line conveniences"
-yay --sync --refresh --needed --noconfirm \
-  python-pygments \
-  ttf-meslo-nerd-font-powerlevel10k \
-  zsh \
-  zsh-autosuggestions \
-  zsh-completions \
-  zshdb \
-  zsh-doc \
-  zsh-history-substring-search \
-  zsh-lovers \
-  zsh-syntax-highlighting \
-  zsh-theme-powerlevel10k
-
-echo "Setting shell control files"
-cp bashrc $HOME/.bashrc
-cp bash_aliases $HOME/.bash_aliases
-cp vimrc-dark $HOME/.vimrc
-sudo cp vimrc-dark /root/.vimrc
-cp zshrc $HOME/.zshrc
-cp p10k.zsh $HOME/.p10k.zsh
-
-echo "Setting default shell for $USER to zsh"
-sudo usermod --shell /usr/bin/zsh $USER
-
-echo "Creating $HOME/.local/bin and $HOME/bin"
-mkdir --parents $HOME/.local/bin
-mkdir --parents $HOME/bin
-
 echo "Finished"
