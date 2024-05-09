6ceb5178845c8a881a16faf8e43a872e42afadd7
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-05-08 20:44:28 -0700

    ready for final dogfooding pass!!

---
+++ Computer-Music-Distrobox/JupyterLab/2_conda_env.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+set -e
+
+echo "There are two COMPUTE_MODE settings: CUDA for an NVIDIA"
+echo "GPU and CPU for any other system. CUDA is the default."
+echo ""
+echo "To select CPU mode, enter any non-empty string, To stay"
+echo "with the default CUDA, simply press 'Enter'."
+read -p "COMPUTE_MODE?"
+
+if [ "${#REPLY}" -gt "0" ]
+then
+  echo "..Setting COMPUTE_MODE to CPU"
+  export COMPUTE_MODE=CPU
+else
+  echo "..Setting COMPUTE_MODE to CUDA"
+  export COMPUTE_MODE=CUDA
+fi
+echo "COMPUTE_MODE: $COMPUTE_MODE"
+
+source $HOME/mambaforge/etc/profile.d/conda.sh
+
+echo "Creating fresh JupyterLab virtual environment"
+/usr/bin/time conda env create --quiet --yes --file conda-env-$COMPUTE_MODE.yml > ../Logs/2_conda$COMPUTE_MODE.log 2>&1
+
+echo "Activating JupyterLab virtual environment"
+conda activate JupyterLab
+
+echo "Listing virtual environment to JupyterLab-list.log"
+conda list > JupyterLab-list.log
+
+echo "Testing PyTorch, torchaudio and torchvision"
+python ./test-pytorch-$COMPUTE_MODE.py
+python ./test-torchaudio.py
+python ./test-torchvision.py
+
+echo "Installing R package 'IRkernel'"
+Rscript -e "install.packages('IRkernel', quiet = TRUE)"
+
+echo "Installing kernel"
+Rscript -e "IRkernel::installspec()"
+
+echo "Finished"
---
+++ Computer-Music-Distrobox/JupyterLab/test-pytorch-CPU.py
@@ -0,0 +1,9 @@
+# https://chrisalbon.com/code/deep_learning/pytorch/basics/check_if_pytorch_is_using_gpu/
+# https://forums.developer.nvidia.com/t/pytorch-for-jetson-version-1-10-now-available/72048
+
+import torch
+print("torch version", torch.__version__)
+print('CUDA available: ' + str(torch.cuda.is_available()))
+print('cuDNN version: ' + str(torch.backends.cudnn.version()))
+print("CUDA device count:", torch.cuda.device_count())
+print("")
