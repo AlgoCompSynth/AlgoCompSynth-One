d9afca3d112c462c00c9ba3f0a71fc815a045a3f
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-05-10 19:37:07 -0700

    make CPU the default

--- Computer-Music-Distrobox/0_host_recreate_distrobox.sh
+++ Computer-Music-Distrobox/0_host_recreate_distrobox.sh
@@ -3,19 +3,19 @@
 set -e
 
 echo "There are two COMPUTE_MODE settings: CUDA for an NVIDIA"
-echo "GPU and CPU for any other system. CUDA is the default."
+echo "GPU and CPU for any other system. CPU is the default."
 echo ""
-echo "To select CPU mode, enter any non-empty string, To stay"
-echo "with the default CUDA, simply press 'Enter'."
+echo "To select CUDA mode, enter any non-empty string, To stay"
+echo "with the default CPU, simply press 'Enter'."
 read -p "COMPUTE_MODE?"
 
 if [ "${#REPLY}" -gt "0" ]
 then
-  echo "..Setting COMPUTE_MODE to CPU"
-  export COMPUTE_MODE=CPU
-else
   echo "..Setting COMPUTE_MODE to CUDA"
   export COMPUTE_MODE=CUDA
+else
+  echo "..Setting COMPUTE_MODE to CPU"
+  export COMPUTE_MODE=CPU
 fi
 echo "\$COMPUTE_MODE: $COMPUTE_MODE"
 
