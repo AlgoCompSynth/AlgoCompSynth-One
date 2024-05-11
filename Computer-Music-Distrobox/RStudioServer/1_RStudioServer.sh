b01e3b4157667d5429c2402c96ef05bf806ca125
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2024-05-11 12:16:36 -0700

    Install R on either Bookworm or Ubuntu

--- Computer-Music-Distrobox/RStudioServer/1_RStudioServer.sh
+++ Computer-Music-Distrobox/RStudioServer/1_RStudioServer.sh
@@ -2,19 +2,48 @@
 
 set -e
 
-echo "Getting signing key"
-# https://cran.rstudio.com/bin/linux/debian/#secure-apt
-gpg --keyserver keyserver.ubuntu.com \
-  --recv-key '95C0FAF38DB3CCAD0C080A7BDC78B2DDEABC47B7'
-gpg --armor --export '95C0FAF38DB3CCAD0C080A7BDC78B2DDEABC47B7' | \
-  sudo tee /etc/apt/trusted.gpg.d/cran_debian_key.asc
+DISTRIBUTOR=`lsb_release -is | grep -v "No LSB modules"`
+CODENAME=`lsb_release -cs | grep -v "No LSB modules"`
 
-echo "Adding CRAN repository"
-# https://cran.rstudio.com/bin/linux/debian/#debian-bookworm
-sudo cp bookworm.list /etc/apt/sources.list.d/
-sudo apt-get update -qq
+echo ""
+echo "Running on $DISTRIBUTOR $CODENAME"
+
+if [ "$CODENAME" == "bookworm" ]
+then
+  # https://cran.rstudio.com/bin/linux/debian/#secure-apt
+  echo "..getting signing key"
+  gpg --keyserver keyserver.ubuntu.com \
+    --recv-key '95C0FAF38DB3CCAD0C080A7BDC78B2DDEABC47B7'
+  gpg --armor --export '95C0FAF38DB3CCAD0C080A7BDC78B2DDEABC47B7' | \
+    sudo tee /etc/apt/trusted.gpg.d/cran_debian_key.asc
+
+  echo "..adding CRAN repository"
+  # https://cran.rstudio.com/bin/linux/debian/#debian-bookworm
+  sudo cp bookworm.list /etc/apt/sources.list.d/
+elif [ "$DISTRIBUTOR" == "Ubuntu" ]
+then
+  # https://cran.rstudio.com/bin/linux/ubuntu/ 
+
+  # update indices
+  sudo apt-get update -qq
+  # install two helper packages we need
+  sudo apt-get install -qqy --no-install-recommends software-properties-common dirmngr
+  # add the signing key (by Michael Rutter) for these repos
+  # To verify key, run gpg --show-keys /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc 
+  # Fingerprint: E298A3A825C0D65DFD57CBB651716619E084DAB9
+  echo "..getting signing key"
+  wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc \
+    | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+  # add the R 4.0 repo from CRAN -- adjust 'focal' to 'groovy' or 'bionic' as needed
+  echo "..adding CRAN repository"
+  sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $CODENAME-cran40/"
+else
+  echo "..exit -1024 unrecognized distributor / codename"
+  exit -1024
+fi
 
 echo "Installing R and gdebi-core"
+sudo apt-get update -qq
 sudo apt-get install -qqy --no-install-recommends \
   gdebi-core \
   r-base \
